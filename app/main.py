b2c4888828d8575bbf0fa72d036d81ddc0e4c43b
Elias <elias.leguizamon1997@gmail.com>
2025-06-27 17:51:07 -0300

    FEATURE: Add CORS Pollicy for integrate frontend

--- app/main.py
+++ app/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
-from sqlmodel import Session
-from sqlalchemy import text
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.database.config import engine, create_db_and_tables
 from app.routes import ping, messages, history
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(ping.router, prefix="/ping")
 app.include_router(messages.router, prefix="/messages")
